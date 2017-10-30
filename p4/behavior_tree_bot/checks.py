cec4ccae873926bf238fec5a07139ce787038a2c
JoshuaNavarro <joanavar@ucsc.edu>
2017-10-29 22:48:03 -0700

    Updated checks.py to get second strongest planet. (Hopefully).

--- p4/behavior_tree_bot/checks.py
+++ p4/behavior_tree_bot/checks.py
@@ -13,7 +13,7 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
-        
+
 # returns true if enemy has less than or equal to blitz_min_planets        
 def enemy_owns_few_planets(state):
   # check only planets we are not already attacking
@@ -27,27 +27,34 @@ def enemy_owns_few_planets(state):
 
   # returns true if our fleet average is over the agressive_fleet_min
   # note this only checks standby fleets, not active ones
-  def fleet_is_strong(state):
+def fleet_is_strong(state):
     my_ship_count = sum(planet.num_ships for planet in state.my_planets())
     my_planet_count = sum(planet for planet in state.my_planets())
     my_ship_count_average = my_ship_count/my_planet_count
-    
+
     if my_ship_count_average > agressive_fleet_min:
       return True
     else:
       return False
     
   # returns true if we can take largest enemy planet with our one/two strongest planets
-  def can_take_largest_enemy_planet(state):
+def can_take_largest_enemy_planet(state):
     largest_enemy_planet_count = max(planet.num_ships for planet in state.enemy_planets())
     my_largest_planet_count = max(planet.num_ships for planet in state.my_planets())
-    
+    second_strongest=-999;
+
+    #Get second strongest planet.
+    for planet in state.my_planets():
+       if(planet.num_ships!=my_largest_planet_count and planet.num_ships>second_strongest): second_strongest=planet.num_ships;
+
     if my_largest_planet_count*max_allowable_send_percentage > largest_enemy_planet_count:
       return True
+
     else:
       # take this and the next strongest planet
-      my_largest_planet_count = my_largest_planet_count + max(planet.num_ships != my_largest_planet_count for planet in state.my_planets(), 0)
+      my_largest_planet_count = my_largest_planet_count + second_strongest;
       if my_largest_planet_count*max_allowable_send_percentage > largest_enemy_planet_count:
         return True
       else:
         return False
+
